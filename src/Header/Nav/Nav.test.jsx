d08c506be2ac758ec11088fee08f575cd71d1ef4
Ian Routledge <ian.routledge@nice.org.uk>
2019-02-04 15:51:53 +0000

    Fix tests

--- src/Header/Nav/Nav.test.jsx
+++ src/Header/Nav/Nav.test.jsx
@@ -4,12 +4,16 @@ import { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 
 describe("Nav", () => {
+	const defaultProps = {
+		isLoggedIn: false
+	};
+
 	it("Renders without crashing", () => {
-		const wrapper = shallow(<Nav />);
+		const wrapper = shallow(<Nav {...defaultProps} />);
 		expect(wrapper).toHaveLength(1);
 	});
 	it("Matches snapshot", () => {
-		const wrapper = shallow(<Nav />);
+		const wrapper = shallow(<Nav {...defaultProps} />);
 		expect(toJson(wrapper)).toMatchSnapshot();
 	});
 });
