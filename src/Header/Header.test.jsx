b23b8a9fc847d7840179395b96223e19eb3e7ea1
Ian Routledge <ian.routledge@nice.org.uk>
2019-02-08 14:39:29 +0000

    Add tests for mobile menu button

--- src/Header/Header.test.jsx
+++ src/Header/Header.test.jsx
@@ -13,4 +13,34 @@ describe("Header", () => {
 		const wrapper = shallow(<Header {...defaultProps} />);
 		expect(wrapper).toHaveLength(1);
 	});
+
+	describe("Mobile menu button", () => {
+		it("Mobile menu button is collapsed by default", () => {
+			const wrapper = shallow(<Header {...defaultProps} />);
+			expect(wrapper.find("button").props()["aria-expanded"]).toEqual(false);
+			expect(wrapper.find("button").text()).toEqual("Menu");
+		});
+		it("Mobile menu button toggles text and aria-expanded on click", () => {
+			const wrapper = shallow(<Header {...defaultProps} />);
+			wrapper.find("button").simulate("click");
+			expect(wrapper.find("button").props()["aria-expanded"]).toEqual(true);
+			expect(wrapper.find("button").text()).toEqual("Close");
+		});
+		it("Mobile menu button toggles nav on click", () => {
+			const wrapper = shallow(<Header {...defaultProps} />);
+
+			wrapper.find("button").simulate("click");
+			expect(wrapper.find("Nav").props().isExpanded).toEqual(true);
+
+			wrapper.find("button").simulate("click");
+			expect(wrapper.find("Nav").props().isExpanded).toEqual(false);
+		});
+	});
+
+	describe("Nav", () => {
+		it("Nav is collapsed by default", () => {
+			const wrapper = shallow(<Header {...defaultProps} />);
+			expect(wrapper.find("Nav").props().isExpanded).toEqual(false);
+		});
+	});
 });
