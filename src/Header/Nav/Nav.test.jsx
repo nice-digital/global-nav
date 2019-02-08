8fc777e58d345faae75ee87164999678281f4773
Ian Routledge <ian.routledge@nice.org.uk>
2019-02-08 09:06:26 +0000

    Add test for aria-current=page for navigation

--- src/Header/Nav/Nav.test.jsx
+++ src/Header/Nav/Nav.test.jsx
@@ -3,6 +3,8 @@ import Nav from "./Nav";
 import { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 
+import links from "./__mocks__/links.json";
+
 jest.mock("./links.json", () => require("./__mocks__/links.json"));
 
 describe("Nav", () => {
@@ -33,14 +35,31 @@ describe("Nav", () => {
 		expect(toJson(wrapper.find("ul"))).toMatchSnapshot();
 	});
 
-	it("Adds aria current atrribute for selected service", () => {
-		const wrapper = shallow(<Nav {...defaultProps} service="link2" />);
+	it("Adds aria-current=true atrribute for selected service", () => {
+		const wrapper = shallow(<Nav {...defaultProps} service={links[1].id} />);
 		expect(
 			wrapper
 				.find("a")
 				.at(1)
 				.props()["aria-current"]
 		).toEqual(true);
+	});
+
+	it("Adds aria-current=page atrribute for selected service when matches current URL", () => {
+		const oldLocation = global.window.location;
+		delete global.window.location;
+		global.window.location = new URL(links[1].href);
+
+		const wrapper = shallow(<Nav {...defaultProps} service={links[1].id} />);
+		expect(
+			wrapper
+				.find("a")
+				.at(1)
+				.props()["aria-current"]
+		).toEqual("page");
+
+		// Tidy up
+		global.window.location = oldLocation;
 	});
 
 	it("Adds lastAccountsMenuItem css class to the last Accounts menu item", () => {
