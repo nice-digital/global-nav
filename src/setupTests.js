e74905c5b5f8a682d108dacc1457a968a760d6fb
Ian Routledge <ian.routledge@nice.org.uk>
2019-03-08 16:36:21 +0000

    Enable overriding useragent in tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,3 +2,20 @@ import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
+
+// Allow setting user agent in tests
+// https://github.com/facebook/jest/issues/717#issuecomment-246471809
+Object.defineProperty(
+	window.navigator,
+	"userAgent",
+	(function(_value) {
+		return {
+			get: function _get() {
+				return _value;
+			},
+			set: function _set(v) {
+				_value = v;
+			}
+		};
+	})(window.navigator.userAgent)
+);
