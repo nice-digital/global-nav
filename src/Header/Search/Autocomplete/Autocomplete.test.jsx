4c84f98d3e9555355d552af15c8f30260d3cf959
Ian Routledge <ian.routledge@nice.org.uk>
2019-04-30 11:38:07 +0100

    GN-13 Add tracking for autocomplete selection

--- src/Header/Search/Autocomplete/Autocomplete.test.jsx
+++ src/Header/Search/Autocomplete/Autocomplete.test.jsx
@@ -5,6 +5,8 @@ import Autocomplete from "./Autocomplete";
 import { shallow, mount } from "enzyme";
 import toJson from "enzyme-to-json";
 
+import { eventName, eventTimeout } from "./../../../tracker";
+
 const IE8UserAgent =
 	"Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)";
 
@@ -44,6 +46,66 @@ describe("Autocomplete", () => {
 					.querySelector("input#autocomplete")
 					.getAttribute("data-hj-whitelist")
 			).toEqual("");
+		});
+
+		it("should push autocomplete select event to the dataLayer", () => {
+			document.body.innerHTML = "";
+			var appContainer = document.createElement("div");
+			document.body.appendChild(appContainer);
+
+			const option = {
+				Title: "diabetes type 1",
+				Link: "https://www.nice.org.uk/diabetes1.html"
+			};
+
+			const wrapper = mount(
+				<Autocomplete {...defaultProps} source={[option]} query="diab" />,
+				{ attachTo: appContainer }
+			);
+
+			wrapper
+				.find("#autocomplete")
+				.first()
+				.props()
+				.onConfirm(option);
+
+			expect(window.dataLayer).toEqual([
+				{
+					event: eventName,
+					eventCategory: "Search",
+					eventAction: "Typeahead select",
+					eventLabel: option.Title + " | diab",
+					eventCallback: expect.any(Function),
+					eventTimeout: eventTimeout
+				}
+			]);
+		});
+
+		it("should navigate to selected option in event callback", () => {
+			document.body.innerHTML = "";
+			var appContainer = document.createElement("div");
+			document.body.appendChild(appContainer);
+
+			const option = {
+				Title: "diabetes type 1",
+				Link: "https://www.nice.org.uk/diabetes1.html"
+			};
+
+			const wrapper = mount(
+				<Autocomplete {...defaultProps} source={[option]} query="diab" />,
+				{ attachTo: appContainer }
+			);
+
+			wrapper
+				.find("#autocomplete")
+				.first()
+				.props()
+				.onConfirm(option);
+
+			window.dataLayer[0].eventCallback();
+			expect(window.location.href).toEqual(
+				"https://www.nice.org.uk/diabetes1.html"
+			);
 		});
 	});
 
