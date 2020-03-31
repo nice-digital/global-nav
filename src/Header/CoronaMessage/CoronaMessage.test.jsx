5d8d1c2abfa661019e109234a2d40f02cbc8be1d
w@rren <warren.keith@nice.org.uk>
2020-03-31 10:32:42 +0100

    GN-42 Add ability to minimise COVID banner with cookie storage (#60)
    * GN-000 Add cookie dismiss to the COVID banner
    
    * GN-000 Minimise COVID banner to a thin link
    
    * GN-42 Fix test
    
    Co-authored-by: Ian Routledge <ian.routledge@nice.org.uk>

--- src/Header/CoronaMessage/CoronaMessage.test.jsx
+++ src/Header/CoronaMessage/CoronaMessage.test.jsx
@@ -1,17 +1,124 @@
 import React from "react";
-import CoronaMessage from "./CoronaMessage";
-import { shallow } from "enzyme";
+import Cookies from "js-cookie";
 import toJson from "enzyme-to-json";
+import CoronaMessage, {
+	CookieMessageVersion,
+	CookieName
+} from "./CoronaMessage";
+import { shallow } from "enzyme";
 
 describe("CoronaMessage", () => {
+	const oldCookie = document.cookie;
+
+	beforeEach(() => {
+		// Reset overridden cookie property
+		document.cookie = oldCookie;
+		Cookies.set(CookieName, null);
+	});
+
 	it("Renders without crashing", () => {
 		const wrapper = shallow(<CoronaMessage />);
+
 		expect(wrapper).toHaveLength(1);
 	});
 
-	it("Matches snapshot", () => {
-		const wrapper = shallow(<CoronaMessage />);
+	it("Renders only thin link by default/on the server", () => {
+		const wrapper = shallow(<CoronaMessage />, {
+			disableLifecycleMethods: true
+		});
 
 		expect(toJson(wrapper)).toMatchSnapshot();
+	});
+
+	it("Renders after rehydration on the client", () => {
+		const wrapper = shallow(<CoronaMessage />);
+
+		wrapper.update();
+
+		expect(wrapper.isEmptyRender()).toBe(false);
+	});
+
+	it("Matches snapshot for new visitor", () => {
+		const wrapper = shallow(<CoronaMessage />);
+
+		wrapper.update();
+
+		expect(toJson(wrapper)).toMatchSnapshot();
+	});
+
+	it("Hides full notice when seen correct message version", () => {
+		Cookies.set(CookieName, CookieMessageVersion);
+
+		const wrapper = shallow(<CoronaMessage />);
+
+		wrapper.update();
+
+		expect(wrapper.find("p").text()).toEqual(
+			"Read about the NICE response to Coronavirus (COVID-19)"
+		);
+	});
+
+	it("Triggers click handler on close button click", () => {
+		const handleClick = jest.spyOn(CoronaMessage.prototype, "handleClick");
+		const wrapper = shallow(<CoronaMessage />);
+
+		wrapper.update();
+
+		wrapper
+			.find("button")
+			.at(0)
+			.simulate("click");
+
+		expect(handleClick).toHaveBeenCalledTimes(1);
+	});
+
+	it("Hides message on close button click", () => {
+		const wrapper = shallow(<CoronaMessage />);
+
+		wrapper.update();
+
+		wrapper
+			.find("button")
+			.at(0)
+			.simulate("click");
+
+		wrapper.update();
+
+		expect(wrapper.find("p").text()).toEqual(
+			"Read about the NICE response to Coronavirus (COVID-19)"
+		);
+	});
+
+	it("Stores cookie on evidence domain on button click", () => {
+		const oldLocation = global.window.location;
+		delete global.window.location;
+		global.window.location = new URL("https://www.evidence.nhs.uk/");
+
+		const cookieSet = jest.fn();
+
+		Object.defineProperty(document, "cookie", {
+			get: jest.fn().mockImplementation(() => null),
+			set: cookieSet,
+			configurable: true
+		});
+
+		const wrapper = shallow(<CoronaMessage />);
+
+		wrapper.update();
+
+		wrapper
+			.find("button")
+			.at(0)
+			.simulate("click");
+
+		const date = new Date();
+		date.setDate(date.getDate() + 365);
+
+		expect(cookieSet.mock.calls[1][0]).toEqual(
+			`${CookieName}=${CookieMessageVersion}; path=/; expires=${date.toUTCString()}; domain=www.evidence.nhs.uk`
+		);
+
+		// Tidy up
+		global.window.location = oldLocation;
 	});
 });
