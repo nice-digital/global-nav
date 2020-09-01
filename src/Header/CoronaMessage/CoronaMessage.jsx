9f1e7970bad0bcb499e32681b33ea7cf1b4218fa
1996dylanriley <dylan.riley@nice.org.uk>
2020-09-01 13:41:21 +0100

    GN-62 Minimize covid banner (#79)
    * GN-62 Minimize covid banner
    
    * GN-62 Styling for minimized covid header
    
    * GN-62 Remove obsolete covid banner tests
    
    * GN-62 Remove more obsolete code
    
    * GN-62 Add snapshot test
    
    * GN-62 Hide banner on print

--- src/Header/CoronaMessage/CoronaMessage.jsx
+++ src/Header/CoronaMessage/CoronaMessage.jsx
@@ -1,122 +1,21 @@
 import React, { Component } from "react";
-import Cookies from "js-cookie";
 import classNames from "classnames";
-import PropTypes from "prop-types";
 
 import styles from "./CoronaMessage.module.scss";
 
-export const CookieName = "seen_corona_message";
-
-// Increment this and release a new version to make the message reappear for users who had previously dismissed
-export const CookieMessageVersion = 1;
-
 class CoronaMessage extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			hasSeenPreviousVersion: false,
-			isMinimised: true,
-			canUseDOM: false
-		};
-
-		this.handleClick = this.handleClick.bind(this);
-	}
-
-	componentDidMount() {
-		this.setState({
-			canUseDOM: true
-		});
-
-		const cookieValue = Cookies.getJSON(CookieName);
-
-		if (!cookieValue) {
-			Cookies.remove(CookieName);
-			this.setState({
-				hasSeenPreviousVersion: false,
-				isMinimised: false // Hide by default on the assumption that if JS doesn't work then we won't be setting cookies anyway
-			});
-		} else {
-			const seenVersion = cookieValue;
-
-			this.setState({
-				hasSeenPreviousVersion: seenVersion < CookieMessageVersion,
-				isMinimised: seenVersion === CookieMessageVersion
-			});
-		}
-	}
-
-	getCookieDomain(host) {
-		return ~host.indexOf("nice.org.uk") ? "nice.org.uk" : host;
-	}
-
-	handleClick() {
-		const cookieOptions = {
-			secure: false,
-			expires: 365 // In days
-		};
-
-		if (window.location.hostname !== "localhost") {
-			cookieOptions.domain = this.getCookieDomain(window.location.hostname);
-		}
-
-		Cookies.set(CookieName, CookieMessageVersion, cookieOptions);
-
-		this.setState({
-			isMinimised: true
-		});
-
-		const { onResize } = this.props;
-		if (onResize) {
-			const onResizeCallback =
-				typeof onResize === "function" ? onResize : window[onResize];
-
-			if (typeof onResizeCallback === "function") {
-				onResizeCallback();
-			}
-		}
-	}
-
 	render() {
-		const classes = classNames(
-			styles.wrapper,
-			this.state.isMinimised && styles.minimised
-		);
+		const classes = classNames(styles.wrapper);
 
 		return (
 			<aside className={classes}>
 				<div className={styles.container}>
-					{this.state.isMinimised ? (
-						<p className={styles.minimised}>
-							Read about{" "}
-							<a href="https://www.nice.org.uk/covid-19">
-								our approach to COVID-19
-							</a>
-						</p>
-					) : (
-						<>
-							<h2>Coronavirus (COVID-19)</h2>
-							<p>
-								For information on how NICE is supporting the NHS and social
-								care, view our new{" "}
-								<a href="https://www.nice.org.uk/coronavirus">
-									rapid guidelines and evidence&nbsp;summaries
-								</a>
-								. Learn about the{" "}
-								<a href="https://gov.uk/coronavirus">
-									government response to coronavirus on&nbsp;GOV.UK
-								</a>
-								.
-							</p>
-							<button
-								className={styles.button}
-								type="button"
-								onClick={this.handleClick}
-							>
-								Close
-							</button>
-						</>
-					)}
+					<p>
+						Read about{" "}
+						<a href="https://www.nice.org.uk/covid-19">
+							our approach to COVID-19
+						</a>
+					</p>
 				</div>
 			</aside>
 		);
@@ -124,7 +23,3 @@ class CoronaMessage extends Component {
 }
 
 export default CoronaMessage;
-
-CoronaMessage.propTypes = {
-	onResize: PropTypes.oneOfType([PropTypes.func, PropTypes.string])
-};
