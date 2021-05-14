501d78ee50a15350ad6384b10c841e320ab045de
Ian Routledge <ian@ediblecode.com>
2021-05-14 15:01:14 +0100

    GN-89 Export all types

--- types.d.ts
+++ types.d.ts
@@ -1,25 +1,28 @@
 declare module "@nice-digital/global-nav" {
 	import React = require("react");
 
-	type AutoCompleteSuggestion = {
+	export type AutoCompleteSuggestion = {
 		Title: string;
 		TypeAheadType?: string;
 		TitleHtml?: string;
 		Link: string;
 	};
 
-	type OnSearchingCallback = (args: { query: string }) => void;
+	export type OnSearchingCallback = (args: { query: string }) => void;
 
-	type OnResizeCallback = () => void;
+	export type OnResizeCallback = () => void;
 
-	type AutoCompleteSuggestions = false | string | AutoCompleteSuggestion[];
+	export type AutoCompleteSuggestions =
+		| false
+		| string
+		| AutoCompleteSuggestion[];
 
-	type AutoCompleteOptions = {
+	export type AutoCompleteOptions = {
 		suggestions: AutoCompleteSuggestions;
 		suggestionTemplate: (suggestion: AutoCompleteSuggestion) => string;
 	};
 
-	type SearchProps = {
+	export type SearchProps = {
 		url?: string;
 		autocomplete?: AutoCompleteSuggestions | AutoCompleteOptions;
 		placeholder?: string;
@@ -28,7 +31,7 @@ declare module "@nice-digital/global-nav" {
 		onResize?: string | OnResizeCallback;
 	};
 
-	type Service =
+	export type Service =
 		| "pathways"
 		| "guidance"
 		| "standards"
@@ -38,12 +41,12 @@ declare module "@nice-digital/global-nav" {
 		| "cks"
 		| "journals";
 
-	type Link = {
+	export type Link = {
 		text: string;
 		url: string;
 	};
 
-	type OnNavigatingCallback = (e: {
+	export type OnNavigatingCallback = (e: {
 		element: HTMLAnchorElement;
 		href: string;
 	}) => void;
@@ -59,7 +62,7 @@ declare module "@nice-digital/global-nav" {
 		environment?: "live" | "beta" | "test" | "local";
 	};
 
-	type HeaderProps = {
+	export type HeaderProps = {
 		service?: Service;
 		skipLinkId?: string;
 		search?: false | SearchProps;
@@ -67,7 +70,7 @@ declare module "@nice-digital/global-nav" {
 		onNavigating?: string | OnNavigatingCallback;
 	};
 
-	type FooterProps = {
+	export type FooterProps = {
 		service?: Service;
 	};
 
