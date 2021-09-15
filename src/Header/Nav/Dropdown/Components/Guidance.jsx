8a115240e938f5997b879f9c8b0c3d5fb62a2dd1
Eleanor Mollett <eleanor.mollett@gmail.com>
2021-09-15 13:36:58 +0100

    GN-101 Use bundled design system css

--- src/Header/Nav/Dropdown/Components/Guidance.jsx
+++ src/Header/Nav/Dropdown/Components/Guidance.jsx
@@ -1,75 +1,26 @@
 import React from "react";
 import ReactShadowRoot from "react-shadow-root";
 
-const styles = `
-	h2{  clear: both;
-		font-weight: 700;
-		font-size: 2.25rem;
-		line-height: 2.5rem;
-		margin-bottom: 1rem;
-		margin-top: 0rem;
-		max-width: 66ch;
-		font-family: Lato,Helvetica Neue,Helvetica,Arial,sans-serif;}
-	h3{  clear: both;
-		font-weight: 700;
-		font-size: 1.75rem;
-		line-height: 2.5rem;
-		margin-bottom: 1rem;
-		margin-top: 2rem;
-		max-width: 66ch;
-		font-family: Lato,Helvetica Neue,Helvetica,Arial,sans-serif;}
-	button{
-		background: #451551;
-		border: .0625em solid #fff;
-		color: #fff;
-		display: inline-block;
-		font-size: 1rem;
-		line-height: 1.5rem;
-		margin: 0 .5em .5em -.25em;
-		padding: .5em 1em;
-		position: relative;
-		text-align: left;
-		text-decoration: none;
-		vertical-align: top;
-		white-space: nowrap;}
-	a{
-		color: #005ea5;
-		line-height: 1.25;
-		text-decoration: none}
-	ul{
-		list-style: none;
-		margin-left: 0;
-		padding: 0;
-	}
-	.grid{
-		display: flex;
-		flex-direction: row;
-		justify-content: flex-start;
-		column-gap: 2rem;
-		flex-wrap: wrap;
-	}
-	.item{
-		flex: 1;
-	}`;
-
 export function Guidance() {
 	return (
 		<section aria-label="NICE Guidance - dropdown navigation">
 			<ReactShadowRoot>
-				<style>{styles}</style>
-				<h2>Guidance</h2>
+				<link href="https://nds-css.netlify.app/bundle.css" rel="stylesheet" />
+				<h2 className="mt--0">Guidance</h2>
 				<p>
 					Evidence-based recommendations developed by independent committees,
 					including professionals and lay members, and consulted on by
 					stakeholder.
 				</p>
-				<button to="/">Find NICE Guidance</button>
+				<button className="btn btn--cta" to="/">
+					Find NICE Guidance
+				</button>
 
 				<div className="grid">
-					<div className="item">
+					<div data-g="12 md:6">
 						<h3>Most visited guidance topics</h3>
 						<p>Guidance grouped by subject, most visited by other people:</p>
-						<ul style={{ columnCount: 2 }}>
+						<ul className="list list--unstyled" style={{ columnCount: 2 }}>
 							<li>
 								<a href="/">Diabetes</a>
 							</li>
@@ -96,10 +47,10 @@ export function Guidance() {
 							</li>
 						</ul>
 					</div>
-					<div className="item">
+					<div data-g="12 md:6">
 						<h3>COVID-19</h3>
 						<p>Latest COVID-19 guidance:</p>
-						<ul>
+						<ul className="list list--unstyled">
 							<li>
 								<a href="/">
 									COVID-19 rapid guideline: vaccine-induced immune
--- src/Header/Nav/Dropdown/Components/More.jsx
+++ src/Header/Nav/Dropdown/Components/More.jsx
@@ -1,52 +1,13 @@
 import React from "react";
 import ReactShadowRoot from "react-shadow-root";
 
-const styles = `
-	h3{  clear: both;
-		font-weight: 700;
-		font-size: 1.75rem;
-		line-height: 2.5rem;
-		margin-bottom: 1rem;
-		margin-top: 2rem;
-		max-width: 66ch;
-		font-family: Lato,Helvetica Neue,Helvetica,Arial,sans-serif;}
-	button{
-		background: #451551;
-		border: .0625em solid #fff;
-		color: #fff;
-		display: inline-block;
-		font-size: 1rem;
-		line-height: 1.5rem;
-		margin: 0 .5em .5em -.25em;
-		padding: .5em 1em;
-		position: relative;
-		text-align: left;
-		text-decoration: none;
-		vertical-align: top;
-		white-space: nowrap;}
-	ul{
-		list-style: none;
-		margin-left: 0;
-		padding: 0;
-	}
-	.grid{
-		display: flex;
-		flex-direction: row;
-		justify-content: flex-start;
-		column-gap: 2rem;
-		flex-wrap: wrap;
-	}
-	.item{
-		flex: 1;
-	}`;
-
 export function More() {
 	return (
 		<section aria-label="More NICE services">
 			<ReactShadowRoot>
-				<style>{styles}</style>
+				<link href="https://nds-css.netlify.app/bundle.css" rel="stylesheet" />
 				<div className="grid">
-					<div className="item">
+					<div data-g="12 md:4">
 						<h3>Evidence search</h3>
 						<p>
 							Evidence search helps you make better, faster evidence-based
@@ -55,10 +16,12 @@ export function More() {
 							go.
 						</p>
 						<p>
-							<button to="https://evidence.nhs.uk">Evidence search</button>
+							<button className="btn btn--cta" to="https://evidence.nhs.uk">
+								Evidence search
+							</button>
 						</p>
 					</div>
-					<div className="item">
+					<div data-g="12 md:4">
 						<h3>Standards and indicators</h3>
 						<p>
 							Quality standards set out the priority areas for quality
@@ -70,12 +33,15 @@ export function More() {
 							<li>improve quality information on how to measure progress.</li>
 						</ul>
 						<p>
-							<button to="https://www.nice.org.uk/standards-and-indicators">
+							<button
+								className="btn btn--cta"
+								to="https://www.nice.org.uk/standards-and-indicators"
+							>
 								Standards and indicators
 							</button>
 						</p>
 					</div>
-					<div className="item">
+					<div data-g="12 md:4">
 						<h3>Journals and databases</h3>
 						<p>
 							Quick access to a range of journals and other evidence-based
@@ -91,7 +57,10 @@ export function More() {
 							resources.
 						</p>
 						<p>
-							<button to="https://www.nice.org.uk/about/what-we-do/evidence-services/journals-and-databases">
+							<button
+								className="btn btn--cta"
+								to="https://www.nice.org.uk/about/what-we-do/evidence-services/journals-and-databases"
+							>
 								Journals and databases
 							</button>
 						</p>
