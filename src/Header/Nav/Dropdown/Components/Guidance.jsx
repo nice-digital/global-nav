1fbcdbcdb1fad3e0d753f42378efe1483fa6db5c
Eleanor Mollett <eleanor.mollett@gmail.com>
2021-09-15 13:02:21 +0100

    GN-101 Styling issues across applications

--- src/Header/Nav/Dropdown/Components/Guidance.jsx
+++ src/Header/Nav/Dropdown/Components/Guidance.jsx
@@ -1,70 +1,121 @@
 import React from "react";
-import { Button } from "@nice-digital/nds-button";
-import { Grid, GridItem } from "@nice-digital/nds-grid";
+import ReactShadowRoot from "react-shadow-root";
+
+const styles = `
+	h2{  clear: both;
+		font-weight: 700;
+		font-size: 2.25rem;
+		line-height: 2.5rem;
+		margin-bottom: 1rem;
+		margin-top: 0rem;
+		max-width: 66ch;
+		font-family: Lato,Helvetica Neue,Helvetica,Arial,sans-serif;}
+	h3{  clear: both;
+		font-weight: 700;
+		font-size: 1.75rem;
+		line-height: 2.5rem;
+		margin-bottom: 1rem;
+		margin-top: 2rem;
+		max-width: 66ch;
+		font-family: Lato,Helvetica Neue,Helvetica,Arial,sans-serif;}
+	button{
+		background: #451551;
+		border: .0625em solid #fff;
+		color: #fff;
+		display: inline-block;
+		font-size: 1rem;
+		line-height: 1.5rem;
+		margin: 0 .5em .5em -.25em;
+		padding: .5em 1em;
+		position: relative;
+		text-align: left;
+		text-decoration: none;
+		vertical-align: top;
+		white-space: nowrap;}
+	a{
+		color: #005ea5;
+		line-height: 1.25;
+		text-decoration: none}
+	ul{
+		list-style: none;
+		margin-left: 0;
+		padding: 0;
+	}
+	.grid{
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+		column-gap: 2rem;
+		flex-wrap: wrap;
+	}
+	.item{
+		flex: 1;
+	}`;
 
 export function Guidance() {
 	return (
 		<section aria-label="NICE Guidance - dropdown navigation">
-			<h2 className="mt--0">Guidance</h2>
-			<p>
-				Evidence-based recommendations developed by independent committees,
-				including professionals and lay members, and consulted on by
-				stakeholder.
-			</p>
-			<Button variant="cta" to="/">
-				Find NICE Guidance
-			</Button>
+			<ReactShadowRoot>
+				<style>{styles}</style>
+				<h2>Guidance</h2>
+				<p>
+					Evidence-based recommendations developed by independent committees,
+					including professionals and lay members, and consulted on by
+					stakeholder.
+				</p>
+				<button to="/">Find NICE Guidance</button>
 
-			<Grid gutter="loose">
-				<GridItem cols={12} md={6}>
-					<h3>Most visited guidance topics</h3>
-					<p>Guidance grouped by subject, most visited by other people:</p>
-					<ul className="list list--unstyled" style={{ columnCount: 2 }}>
-						<li>
-							<a href="/">Diabetes</a>
-						</li>
-						<li>
-							<a href="/">Mental health and wellbeing</a>
-						</li>
-						<li>
-							<a href="/">Depression</a>
-						</li>
-						<li>
-							<a href="/">Chronic obstructive pulmonary disease</a>
-						</li>
-						<li>
-							<a href="/">Stroke and transient ischaemic attack</a>
-						</li>
-						<li>
-							<a href="/">Children and young people</a>
-						</li>
-						<li>
-							<a href="/">Anxiety</a>
-						</li>
-						<li>
-							<a href="/">Medicines management</a>
-						</li>
-					</ul>
-				</GridItem>
-				<GridItem cols={12} md={6}>
-					<h3>COVID-19</h3>
-					<p>Latest COVID-19 guidance:</p>
-					<ul className="list list--unstyled">
-						<li>
-							<a href="/">
-								COVID-19 rapid guideline: vaccine-induced immune
-								thrombocytopenia and thrombosis (VITT)
-							</a>
-						</li>
-						<li>
-							<a href="/">COVID-19 rapid guideline: managing COVID-19</a>
-						</li>
-						<li>
-							<a href="/">View all COVID-19 guidance</a>
-						</li>
-					</ul>
-				</GridItem>
-			</Grid>
+				<div className="grid">
+					<div className="item">
+						<h3>Most visited guidance topics</h3>
+						<p>Guidance grouped by subject, most visited by other people:</p>
+						<ul style={{ columnCount: 2 }}>
+							<li>
+								<a href="/">Diabetes</a>
+							</li>
+							<li>
+								<a href="/">Mental health and wellbeing</a>
+							</li>
+							<li>
+								<a href="/">Depression</a>
+							</li>
+							<li>
+								<a href="/">Chronic obstructive pulmonary disease</a>
+							</li>
+							<li>
+								<a href="/">Stroke and transient ischaemic attack</a>
+							</li>
+							<li>
+								<a href="/">Children and young people</a>
+							</li>
+							<li>
+								<a href="/">Anxiety</a>
+							</li>
+							<li>
+								<a href="/">Medicines management</a>
+							</li>
+						</ul>
+					</div>
+					<div className="item">
+						<h3>COVID-19</h3>
+						<p>Latest COVID-19 guidance:</p>
+						<ul>
+							<li>
+								<a href="/">
+									COVID-19 rapid guideline: vaccine-induced immune
+									thrombocytopenia and thrombosis (VITT)
+								</a>
+							</li>
+							<li>
+								<a href="/">COVID-19 rapid guideline: managing COVID-19</a>
+							</li>
+							<li>
+								<a href="/">View all COVID-19 guidance</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</ReactShadowRoot>
 		</section>
 	);
 }
--- src/Header/Nav/Dropdown/Components/More.jsx
+++ src/Header/Nav/Dropdown/Components/More.jsx
@@ -1,70 +1,103 @@
 import React from "react";
-import { Button } from "@nice-digital/nds-button";
-import { Grid, GridItem } from "@nice-digital/nds-grid";
+import ReactShadowRoot from "react-shadow-root";
+
+const styles = `
+	h3{  clear: both;
+		font-weight: 700;
+		font-size: 1.75rem;
+		line-height: 2.5rem;
+		margin-bottom: 1rem;
+		margin-top: 2rem;
+		max-width: 66ch;
+		font-family: Lato,Helvetica Neue,Helvetica,Arial,sans-serif;}
+	button{
+		background: #451551;
+		border: .0625em solid #fff;
+		color: #fff;
+		display: inline-block;
+		font-size: 1rem;
+		line-height: 1.5rem;
+		margin: 0 .5em .5em -.25em;
+		padding: .5em 1em;
+		position: relative;
+		text-align: left;
+		text-decoration: none;
+		vertical-align: top;
+		white-space: nowrap;}
+	ul{
+		list-style: none;
+		margin-left: 0;
+		padding: 0;
+	}
+	.grid{
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+		column-gap: 2rem;
+		flex-wrap: wrap;
+	}
+	.item{
+		flex: 1;
+	}`;
 
 export function More() {
 	return (
 		<section aria-label="More NICE services">
-			<Grid gutter="loose">
-				<GridItem cols={12} md={4}>
-					<h3>Evidence search</h3>
-					<p>
-						Evidence search helps you make better, faster evidence-based
-						decisions. It includes high quality resources from over 800
-						organisations - like searching your trusted work favourites in one
-						go.
-					</p>
-					<p>
-						<Button variant="cta" to="https://evidence.nhs.uk">
-							Evidence search
-						</Button>
-					</p>
-				</GridItem>
-				<GridItem cols={12} md={4}>
-					<h3>Standards and indicators</h3>
-					<p>
-						Quality standards set out the priority areas for quality improvement
-						in health and social care. They cover areas where there is variation
-						in care. Each standard gives you:
-					</p>
-					<ul>
-						<li>a set of statements to help you</li>
-						<li>improve quality information on how to measure progress.</li>
-					</ul>
-					<p>
-						<Button
-							variant="cta"
-							to="https://www.nice.org.uk/standards-and-indicators"
-						>
-							Standards and indicators
-						</Button>
-					</p>
-				</GridItem>
-				<GridItem cols={12} md={4}>
-					<h3>Journals and databases</h3>
-					<p>
-						Quick access to a range of journals and other evidence-based
-						resources for health and social care staff in England.
-					</p>
-					<p>
-						These resources are provided in partnership with Health Education
-						England and NICE.
-					</p>
+			<ReactShadowRoot>
+				<style>{styles}</style>
+				<div className="grid">
+					<div className="item">
+						<h3>Evidence search</h3>
+						<p>
+							Evidence search helps you make better, faster evidence-based
+							decisions. It includes high quality resources from over 800
+							organisations - like searching your trusted work favourites in one
+							go.
+						</p>
+						<p>
+							<button to="https://evidence.nhs.uk">Evidence search</button>
+						</p>
+					</div>
+					<div className="item">
+						<h3>Standards and indicators</h3>
+						<p>
+							Quality standards set out the priority areas for quality
+							improvement in health and social care. They cover areas where
+							there is variation in care. Each standard gives you:
+						</p>
+						<ul>
+							<li>a set of statements to help you</li>
+							<li>improve quality information on how to measure progress.</li>
+						</ul>
+						<p>
+							<button to="https://www.nice.org.uk/standards-and-indicators">
+								Standards and indicators
+							</button>
+						</p>
+					</div>
+					<div className="item">
+						<h3>Journals and databases</h3>
+						<p>
+							Quick access to a range of journals and other evidence-based
+							resources for health and social care staff in England.
+						</p>
+						<p>
+							These resources are provided in partnership with Health Education
+							England and NICE.
+						</p>
 
-					<p>
-						You'll need an NHS OpenAthens account to access most of these
-						resources.
-					</p>
-					<p>
-						<Button
-							variant="cta"
-							to="https://www.nice.org.uk/about/what-we-do/evidence-services/journals-and-databases"
-						>
-							Journals and databases
-						</Button>
-					</p>
-				</GridItem>
-			</Grid>
+						<p>
+							You'll need an NHS OpenAthens account to access most of these
+							resources.
+						</p>
+						<p>
+							<button to="https://www.nice.org.uk/about/what-we-do/evidence-services/journals-and-databases">
+								Journals and databases
+							</button>
+						</p>
+					</div>
+				</div>
+			</ReactShadowRoot>
 		</section>
 	);
 }
