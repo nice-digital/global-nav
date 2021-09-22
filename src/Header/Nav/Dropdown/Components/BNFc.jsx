03c3068a4cd4a69bfce397703daf8560976dfa11
Eleanor Mollett <eleanor.mollett@gmail.com>
2021-09-22 08:59:30 +0100

    GN-103 Use var for url

--- src/Header/Nav/Dropdown/Components/BNFc.jsx
+++ src/Header/Nav/Dropdown/Components/BNFc.jsx
@@ -2,37 +2,44 @@ import React from "react";
 import { Button } from "@nice-digital/nds-button";
 import { Grid, GridItem } from "@nice-digital/nds-grid";
 import styles from "./Components.module.scss";
-
-const drugsAtoZ = [
-	{ letter: "A", link: "https://bnfc.nice.org.uk/drug/#A" },
-	{ letter: "B", link: "https://bnfc.nice.org.uk/drug/#B" },
-	{ letter: "C", link: "https://bnfc.nice.org.uk/drug/#C" },
-	{ letter: "D", link: "https://bnfc.nice.org.uk/drug/#D" },
-	{ letter: "E", link: "https://bnfc.nice.org.uk/drug/#E" },
-	{ letter: "F", link: "https://bnfc.nice.org.uk/drug/#F" },
-	{ letter: "G", link: "https://bnfc.nice.org.uk/drug/#G" },
-	{ letter: "H", link: "https://bnfc.nice.org.uk/drug/#H" },
-	{ letter: "I", link: "https://bnfc.nice.org.uk/drug/#I" },
-	{ letter: "J", link: "https://bnfc.nice.org.uk/drug/#J" },
-	{ letter: "K", link: "https://bnfc.nice.org.uk/drug/#K" },
-	{ letter: "L", link: "https://bnfc.nice.org.uk/drug/#L" },
-	{ letter: "M", link: "https://bnfc.nice.org.uk/drug/#M" },
-	{ letter: "N", link: "https://bnfc.nice.org.uk/drug/#N" },
-	{ letter: "O", link: "https://bnfc.nice.org.uk/drug/#O" },
-	{ letter: "P", link: "https://bnfc.nice.org.uk/drug/#P" },
-	{ letter: "Q", link: null },
-	{ letter: "R", link: "https://bnfc.nice.org.uk/drug/#R" },
-	{ letter: "S", link: "https://bnfc.nice.org.uk/drug/#S" },
-	{ letter: "T", link: "https://bnfc.nice.org.uk/drug/#T" },
-	{ letter: "U", link: "https://bnfc.nice.org.uk/drug/#U" },
-	{ letter: "V", link: "https://bnfc.nice.org.uk/drug/#V" },
-	{ letter: "W", link: "https://bnfc.nice.org.uk/drug/#W" },
-	{ letter: "X", link: null },
-	{ letter: "Y", link: null },
-	{ letter: "Z", link: null },
-];
+import services from "../../../../services.json";
 
 export function BNFc() {
+	const baseUrl = services.external.find(
+		(service) => service.text == "BNFC"
+	).href;
+
+	const drugsAtoZurl = baseUrl + "drug/#";
+
+	const drugsAtoZ = [
+		{ letter: "A", link: true },
+		{ letter: "B", link: true },
+		{ letter: "C", link: true },
+		{ letter: "D", link: true },
+		{ letter: "E", link: true },
+		{ letter: "F", link: true },
+		{ letter: "G", link: true },
+		{ letter: "H", link: true },
+		{ letter: "I", link: true },
+		{ letter: "J", link: true },
+		{ letter: "K", link: true },
+		{ letter: "L", link: true },
+		{ letter: "M", link: true },
+		{ letter: "N", link: true },
+		{ letter: "O", link: true },
+		{ letter: "P", link: true },
+		{ letter: "Q", link: false },
+		{ letter: "R", link: true },
+		{ letter: "S", link: true },
+		{ letter: "T", link: true },
+		{ letter: "U", link: true },
+		{ letter: "V", link: true },
+		{ letter: "W", link: true },
+		{ letter: "X", link: false },
+		{ letter: "Y", link: false },
+		{ letter: "Z", link: false },
+	];
+
 	return (
 		<section aria-label="BNFC - dropdown navigation">
 			<h2 className="mt--0">BNFC</h2>
@@ -41,24 +48,22 @@ export function BNFc() {
 				healthcare professionals with sound up-to-date information on the use of
 				medicines for treating children.
 			</p>
-			<Button variant="primary" to="https://bnfc.nice.org.uk/">
+			<Button variant="primary" to={baseUrl}>
 				View BNFC
 			</Button>
 
 			<h3>Drugs A to Z</h3>
 			<ol className={styles.alphabet}>
-				{drugsAtoZ.map((letter) => {
+				{drugsAtoZ.map(({ letter, link }) => {
 					return (
 						<li
-							key={letter.letter}
-							className={`${styles.letter} ${
-								letter.link == null ? styles.chunkyLetter : ""
-							}`}
+							key={letter}
+							className={`${styles.letter} ${link ? "" : styles.chunkyLetter}`}
 						>
-							{letter.link == null ? (
-								<span>{letter.letter}</span>
+							{link ? (
+								<a href={`${drugsAtoZurl}${letter}`}>{letter}</a>
 							) : (
-								<a href={letter.link}>{letter.letter}</a>
+								<span>{letter}</span>
 							)}
 						</li>
 					);
@@ -70,12 +75,10 @@ export function BNFc() {
 					<h3>Browse A to Z by</h3>
 					<ul className={styles.listUnstyled}>
 						<li>
-							<a href="https://bnfc.nice.org.uk/interaction/">Interactions</a>
+							<a href={`${baseUrl}interaction/`}>Interactions</a>
 						</li>
 						<li>
-							<a href="https://bnfc.nice.org.uk/treatment-summary/">
-								Treatment summaries
-							</a>
+							<a href={`${baseUrl}treatment-summary/`}>Treatment summaries</a>
 						</li>
 					</ul>
 				</GridItem>
@@ -83,29 +86,25 @@ export function BNFc() {
 					<h3>Browse by type</h3>
 					<ul className={styles.listUnstyled} style={{ columnCount: 2 }}>
 						<li>
-							<a href="https://bnfc.nice.org.uk/medical-devices/">
-								Medical devices
-							</a>
+							<a href={`${baseUrl}medical-devices/`}>Medical devices</a>
 						</li>
 						<li>
-							<a href="https://bnfc.nice.org.uk/borderline-substance-taxonomy/">
+							<a href={`${baseUrl}borderline-substance-taxonomy/`}>
 								Borderline substances
 							</a>
 						</li>
 						<li>
-							<a href="https://bnfc.nice.org.uk/dental-practitioners-formulary/">
+							<a href={`${baseUrl}dental-practitioners-formulary/`}>
 								Dental practitioners' formulary
 							</a>
 						</li>
 						<li>
-							<a href="https://bnfc.nice.org.uk/nurse-prescribers-formulary/">
+							<a href={`${baseUrl}nurse-prescribers-formulary/`}>
 								Nurse prescribers' formulary
 							</a>
 						</li>
 						<li>
-							<a href="https://bnfc.nice.org.uk/guidance/">
-								Medicines guidance
-							</a>
+							<a href={`${baseUrl}guidance/`}>Medicines guidance</a>
 						</li>
 					</ul>
 				</GridItem>
@@ -113,15 +112,13 @@ export function BNFc() {
 					<h3>What’s new</h3>
 					<ul className={styles.listUnstyled}>
 						<li>
-							<a href="https://bnfc.nice.org.uk/about/changes.html">
-								Latest BNFC
-							</a>
+							<a href={`${baseUrl}about/changes.html`}>Latest BNFC</a>
 						</li>
 					</ul>
 				</GridItem>
 			</Grid>
 			<hr />
-			<a href="https://bnfc.nice.org.uk/about/">About BNFC</a>
+			<a href={`${baseUrl}about/`}>About BNFC</a>
 		</section>
 	);
 }
