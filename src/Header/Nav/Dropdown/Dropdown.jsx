a7c951c5269a199f33b8fd5e0bb5174b59152315
w@rren <w@rren.dev>
2021-10-11 12:32:39 +0100

    Prettier

--- src/Header/Nav/Dropdown/Dropdown.jsx
+++ src/Header/Nav/Dropdown/Dropdown.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./Dropdown.module.scss";
-import { Guidance, More, StandardsAndIndicators, AboutUs, BNF, BNFc } from "./Components/";
+import {
+	Guidance,
+	More,
+	StandardsAndIndicators,
+	AboutUs,
+	BNF,
+	BNFc,
+} from "./Components/";
 import Remove from "@nice-digital/icons/lib/Remove";
 
 export function Dropdown({
