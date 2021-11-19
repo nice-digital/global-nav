6b6e3cd8623c7bd3be7d40c3a4b37e0e29abcf5d
John Davey <john.davey@nice.org.uk>
2021-11-19 15:04:24 +0000

    basic setup

--- src/hooks/useClickOutside.test.jsx
+++ src/hooks/useClickOutside.test.jsx
@@ -1,69 +1,17 @@
 import React from "react";
-import useClickOutside from "./useClickOutside";
-import { shallow, mount } from "enzyme";
-import toJson from "enzyme-to-json";
+import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
 
-const testFunction = jest.fn();
-
-function App() {
-	const testVariable = "test";
-	const { ref } = useClickOutside(testVariable, testFunction);
-
-	// function wasClickedOutside(result) {
-	// 	if (result == true) context.setidOfOpenDropdown(null);
-	// }
-	return (
-		<>
-			<div ref={ref}>
-				<button>inside</button>
-			</div>
-			<button>outside</button>
-		</>
-	);
-}
+// import useClickOutside from "./useClickOutside";
 
 describe("useClickOutside hook", () => {
-	const defaultProps = {};
+	it("Passes a test", () => {
+		const wrapper = shallow(<div></div>);
+		console.log(wrapper.html());
 
-	beforeEach(() => {
-		// window.dataLayer = [];
-	});
-
-	afterAll(() => {
-		// Cleanup
-		// delete window.dataLayer;
-	});
-
-	it("Renders without crashing", () => {
-		const wrapper = shallow(<App {...defaultProps} />);
-		// console.log(wrapper.debug());
-		expect(wrapper).toHaveLength(1);
-	});
-
-	it("Matches snapshot", () => {
-		const wrapper = shallow(<App {...defaultProps} />);
-		expect(toJson(wrapper)).toMatchSnapshot();
-	});
-
-	it("Returns true when button outside of referenced element is clicked", () => {
-		const wrapper = mount(<App {...defaultProps} />);
-		const insideButton = wrapper.findWhere((node) => {
-			return (
-				node.type() === "button" && node.name() && node.text() === "inside"
-			);
-		});
-		const outsideButton = wrapper.findWhere((node) => {
-			return (
-				node.type() === "button" && node.name() && node.text() === "outside"
-			);
-		});
-
-		insideButton.simulate("click");
-		console.log("###", insideButton.debug());
-		console.log("###", outsideButton.debug());
-
-		// expect(testFunction).toHaveBeenCalled();
-
-		expect(true).toBe(false);
+		// document.createElement("div");
+		// ReactDOM.render(<div>hello</div>, document.querySelector("div"));
+		// expect
+		// expect(useClickOutside()).toBeCalled();
 	});
 });
