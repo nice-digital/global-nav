3dae4b1efc59ab56428e9c35cc39b7d7b2ce817a
John Davey <john.davey@nice.org.uk>
2022-02-25 17:09:09 +0000

    GN-168 Update unit test for main component

--- src/Main/Main.test.jsx
+++ src/Main/Main.test.jsx
@@ -1,32 +1,21 @@
 import React from "react";
 
-import { BackToTop } from "./BackToTop";
+import Main from "./Main";
 import { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 
-describe("BackToTop", () => {
+describe("Main", () => {
 	it("Renders without crashing", () => {
-		const wrapper = shallow(<BackToTop />);
+		const wrapper = shallow(<Main></Main>);
 		expect(wrapper).toHaveLength(1);
 	});
 
 	it("Matches snapshot", () => {
-		const wrapper = shallow(<BackToTop service="bnf" />);
-		expect(toJson(wrapper)).toMatchSnapshot();
-	});
-
-	it("Renders a link with the default hash when no scroll target id prop provided", () => {
-		const wrapper = shallow(<BackToTop service="bnf" />);
-		expect(wrapper.find("a[href='#top']").length).toEqual(1);
-	});
-
-	it("Renders a link with the correct hash when a scroll target id prop provided and target exists", () => {
-		let scrollTargetDiv = document.createElement("div");
-		scrollTargetDiv.id = "content-start";
-		document.body.appendChild(scrollTargetDiv);
 		const wrapper = shallow(
-			<BackToTop service="bnf" scrollTargetId="content-start" />
+			<Main>
+				<p>test</p>
+			</Main>
 		);
-		expect(wrapper.find("a[href='#content-start']").length).toEqual(1);
+		expect(toJson(wrapper)).toMatchSnapshot();
 	});
 });
