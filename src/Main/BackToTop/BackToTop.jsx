4ec4f829c5fa824087b5c28a9c945cfb0ade1382
John Davey <john.davey@nice.org.uk>
2022-02-25 13:15:34 +0000

    GN-168 Remove config options from BackToTop link and hardcode to #top

--- src/Main/BackToTop/BackToTop.jsx
+++ src/Main/BackToTop/BackToTop.jsx
@@ -1,19 +1,14 @@
 import React from "react";
 import ChevronUp from "@nice-digital/icons/lib/ChevronUp";
 import { Container } from "@nice-digital/nds-container";
-import PropTypes from "prop-types";
 
 import styles from "./BackToTop.module.scss";
 
-export const BackToTop = function ({ scrollTargetId = "top" }) {
+export const BackToTop = function () {
 	return (
 		<div className={styles.wrapper}>
 			<nav aria-labelledby="back-to-top-link" className={styles.nav}>
-				<a
-					className={styles.anchor}
-					id="back-to-top-link"
-					href={`#${scrollTargetId}`}
-				>
+				<a className={styles.anchor} id="back-to-top-link" href="top">
 					<Container>
 						<ChevronUp /> Back to top
 					</Container>
@@ -21,8 +16,4 @@ export const BackToTop = function ({ scrollTargetId = "top" }) {
 			</nav>
 		</div>
 	);
-};
-
-BackToTop.propTypes = {
-	scrollTargetId: PropTypes.string,
 };
--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import ChevronUp from "@nice-digital/icons/lib/ChevronUp";
-import { Container } from "@nice-digital/nds-container";
+import { BackToTop } from "./BackToTop/BackToTop";
 import PropTypes from "prop-types";
 
 import styles from "./BackToTop.module.scss";
@@ -9,15 +8,7 @@ const Main = function ({ children }) {
 	return (
 		<main className={styles.main}>
 			{children}
-			<div className={styles.wrapper}>
-				<nav aria-labelledby="back-to-top-link" className={styles.nav}>
-					<a className={styles.anchor} id="back-to-top-link" href="#top">
-						<Container>
-							<ChevronUp /> Back to top
-						</Container>
-					</a>
-				</nav>
-			</div>
+			<BackToTop />
 		</main>
 	);
 };
