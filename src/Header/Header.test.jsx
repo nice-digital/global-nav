8323c9bb1013f940be4858d8d5e4efcd7bb8bb4d
John Davey <john.davey@nice.org.uk>
2022-03-01 11:24:20 +0000

    GN-168 Remove unused dependency from Header.test.jsx

--- src/Header/Header.test.jsx
+++ src/Header/Header.test.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./Header";
-import { shallow, mount } from "enzyme";
+import { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 
 import {
