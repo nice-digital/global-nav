a17431e066e3a4e8e94efe82bdb713997b8e57e4
John Davey <john.davey@nice.org.uk>
2022-03-15 12:59:53 +0000

    GN-172 Remove about page from services link list in footer. Add unit test to verify removal

--- src/Footer/Services/Services.test.jsx
+++ src/Footer/Services/Services.test.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Services from "./Services";
+
 import { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 
@@ -13,5 +14,13 @@ describe("Services", () => {
 	it("Matches snapshot", () => {
 		const wrapper = shallow(<Services service="pathways" />);
 		expect(toJson(wrapper)).toMatchSnapshot();
+	});
+
+	it("Does not render the link to about", () => {
+		const wrapper = shallow(<Services service="pathways" />);
+		const list = wrapper.find("ul").children();
+		const aboutElementText = list.find({ text: "About" });
+
+		expect(aboutElementText).toHaveLength(0);
 	});
 });
