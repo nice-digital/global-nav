1c5ec79037090ba3ba55001bc824bb8e381c679f
John Davey <john.davey@nice.org.uk>
2022-03-22 15:04:17 +0000

    GN-174 About dropdown layout wip

--- src/Header/Nav/Dropdown/Components/AboutUs.jsx
+++ src/Header/Nav/Dropdown/Components/AboutUs.jsx
@@ -141,98 +141,100 @@ export function AboutUs() {
 						</li>
 					</ul>
 				</GridItem>
-				<GridItem data-tracking="What we do">
-					<h3 className="mt--d">What we do</h3>
-					<ul className={classnames(styles.listUnstyled, "mt--d")}>
-						<li>
-							<a href={`${baseUrl}/about/nice-communities/social-care`}>
-								Social care
-							</a>
-						</li>
-						<li>
-							<a href={`${baseUrl}/about/what-we-do/life-sciences`}>
-								Life sciences
-							</a>
-						</li>
-						<li>
-							<a href={`${baseUrl}/about/what-we-do/nice-international`}>
-								NICE international
-							</a>
-						</li>
-						<li>
-							<a
-								href={`${baseUrl}/about/what-we-do/our-programmes/patient-safety`}
-							>
-								Patient safety
-							</a>
-						</li>
-						<li>
-							<a
-								href={`${baseUrl}/about/what-we-do/our-programmes/nice-guidance/nice-guidelines/shared-decision-making`}
-							>
-								Shared decision making
-							</a>
-						</li>
-					</ul>
-				</GridItem>
-				<GridItem data-tracking="Into practice">
-					<h3 className="mt--d">Into practice</h3>
-					<ul className={classnames(styles.listUnstyled, "mt--d")}>
-						<li>
-							<a
-								href={`${baseUrl}/about/nice-communities/social-care/quick-guides`}
-							>
-								Social care quick guides
-							</a>
-						</li>
-						<li>
-							<a
-								href={`${baseUrl}/about/what-we-do/into-practice/measuring-the-uptake-of-nice-guidance`}
-							>
-								Measuring the use of NICE guidance
-							</a>
-						</li>
-						<li>
-							<a
-								href={`${baseUrl}/about/what-we-do/into-practice/resource-planner`}
-							>
-								Resource planner
-							</a>
-						</li>
-						<li>
-							<a href={`${baseUrl}/about/what-we-do/into-practice`}>
-								View all Into practice
-							</a>
-						</li>
-					</ul>
-				</GridItem>
-				<GridItem data-tracking="Who we are">
-					<h3 className="mt--d">Who we are</h3>
-					<ul className={classnames(styles.listUnstyled, "mt--d")}>
-						<li>
-							<a href={`${baseUrl}/about/who-we-are/board`}>The board</a>
-						</li>
-						<li>
-							<a
-								href={`${baseUrl}/about/who-we-are/corporate-publications/the-nice-strategy-2021-to-2026`}
-							>
-								The NICE strategy 2021 to 2026
-							</a>
-						</li>
-						<li>
-							<a href={`${baseUrl}/about/who-we-are/corporate-publications`}>
-								Corporate publications
-							</a>
-						</li>
-						<li>
-							<a href={`${baseUrl}/about/who-we-are`}>View all Who we are</a>
-						</li>
-					</ul>
-				</GridItem>
+				<Grid>
+					<GridItem data-tracking="What we do">
+						<h3 className="mt--d">What we do</h3>
+						<ul className={classnames(styles.listUnstyled, "mt--d")}>
+							<li>
+								<a href={`${baseUrl}/about/nice-communities/social-care`}>
+									Social care
+								</a>
+							</li>
+							<li>
+								<a href={`${baseUrl}/about/what-we-do/life-sciences`}>
+									Life sciences
+								</a>
+							</li>
+							<li>
+								<a href={`${baseUrl}/about/what-we-do/nice-international`}>
+									NICE international
+								</a>
+							</li>
+							<li>
+								<a
+									href={`${baseUrl}/about/what-we-do/our-programmes/patient-safety`}
+								>
+									Patient safety
+								</a>
+							</li>
+							<li>
+								<a
+									href={`${baseUrl}/about/what-we-do/our-programmes/nice-guidance/nice-guidelines/shared-decision-making`}
+								>
+									Shared decision making
+								</a>
+							</li>
+						</ul>
+					</GridItem>
+					<GridItem data-tracking="Into practice">
+						<h3 className="mt--d">Into practice</h3>
+						<ul className={classnames(styles.listUnstyled, "mt--d")}>
+							<li>
+								<a
+									href={`${baseUrl}/about/nice-communities/social-care/quick-guides`}
+								>
+									Social care quick guides
+								</a>
+							</li>
+							<li>
+								<a
+									href={`${baseUrl}/about/what-we-do/into-practice/measuring-the-uptake-of-nice-guidance`}
+								>
+									Measuring the use of NICE guidance
+								</a>
+							</li>
+							<li>
+								<a
+									href={`${baseUrl}/about/what-we-do/into-practice/resource-planner`}
+								>
+									Resource planner
+								</a>
+							</li>
+							<li>
+								<a href={`${baseUrl}/about/what-we-do/into-practice`}>
+									View all Into practice
+								</a>
+							</li>
+						</ul>
+					</GridItem>
+					<GridItem data-tracking="Who we are">
+						<h3 className="mt--d">Who we are</h3>
+						<ul className={classnames(styles.listUnstyled, "mt--d")}>
+							<li>
+								<a href={`${baseUrl}/about/who-we-are/board`}>The board</a>
+							</li>
+							<li>
+								<a
+									href={`${baseUrl}/about/who-we-are/corporate-publications/the-nice-strategy-2021-to-2026`}
+								>
+									The NICE strategy 2021 to 2026
+								</a>
+							</li>
+							<li>
+								<a href={`${baseUrl}/about/who-we-are/corporate-publications`}>
+									Corporate publications
+								</a>
+							</li>
+							<li>
+								<a href={`${baseUrl}/about/who-we-are`}>View all Who we are</a>
+							</li>
+						</ul>
+					</GridItem>
+				</Grid>
 			</Grid>
 			<hr />
 			<ul
-				className={classnames(styles.listUnstyled, "mt--d")}
+				className={classnames(styles.inlineList, "mt--d")}
 				data-tracking="Contact us"
 			>
 				<li>
