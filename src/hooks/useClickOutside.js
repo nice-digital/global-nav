fb2b5cb49da0451f9b0c553ea127cd95ffbabf96
John Davey <john.davey@nice.org.uk>
2022-06-08 12:08:47 +0100

    test node type is HTMLElement

--- src/hooks/useClickOutside.js
+++ src/hooks/useClickOutside.js
@@ -3,9 +3,25 @@ import { useEffect, useRef } from "react";
 function useClickOutside(action) {
 	const ref = useRef(null);
 
+	// Returns true if it is a DOM element for IE11
+	// see https://stackoverflow.com/questions/384286/how-do-you-check-if-a-javascript-object-is-a-dom-object
+	function isElement(o) {
+		return typeof HTMLElement === "object"
+			? o instanceof HTMLElement //DOM2
+			: o &&
+					typeof o === "object" &&
+					o !== null &&
+					o.nodeType === 1 &&
+					typeof o.nodeName === "string";
+	}
+
 	const handleClickOutside = (event) => {
 		// detects that the clicked element is not inside the referenced element
-		if (ref.current && !ref.current.contains(event.target)) {
+		if (
+			ref.current &&
+			isElement(ref.current) &&
+			!ref.current.contains(event.target)
+		) {
 			action(true);
 		}
 	};
