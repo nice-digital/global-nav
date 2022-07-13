2f37f803c05254e14a5520ffe18fc921f30c5491
John Davey <john.davey@nice.org.uk>
2022-07-13 08:41:14 +0100

    GN-192 Convert Search class to functional component and apply aria-activedescendant temporary fix. Version bump nds-container and nds-core to remove sass-mq deprecation warnings

--- src/Header/Search/Search.jsx
+++ src/Header/Search/Search.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useCallback } from "react";
 import PropTypes from "prop-types";
 import SearchIcon from "@nice-digital/icons/lib/Search";
 
@@ -8,36 +8,21 @@ import { getCallbackFunction } from "../../utils";
 
 import styles from "./Search.module.scss";
 
-export default class Search extends Component {
-	constructor(props) {
-		super(props);
+const searchInputSelector = "header form[role='search'] [name='q']";
 
-		this.state = {
-			canUseDOM: false,
-		};
-
-		this.searchSubmitHandler = this.searchSubmitHandler.bind(this);
-		this.keyDownHandler = this.keyDownHandler.bind(this);
-	}
-
-	componentDidMount() {
-		this.setState({
-			canUseDOM: true,
-		});
-	}
-
-	keyDownHandler(e) {
+const Search = function (props) {
+	const keyDownHandler = function (e) {
 		// Submit the form when we press enter to allow the enter key functionality:
 		// The accessible-autocomplete doesn't let you use enter by default
 		if (e.key === "Enter" && e.target.id === "autocomplete") {
-			const wasSearchActionOverridden = this.searchSubmitHandler(e);
+			const wasSearchActionOverridden = searchSubmitHandler(e);
 
 			if (!wasSearchActionOverridden) {
 				const searchForm = document.getElementById("global-nav-search-form");
 				if (searchForm) searchForm.submit();
 			}
 		}
-	}
+	};
 
 	/**
 	 * Event handler for the form submit action
@@ -46,10 +31,10 @@ export default class Search extends Component {
 	 * @returns true if the search action was overridden; otherwise false
 	 * @memberof Search
 	 */
-	searchSubmitHandler(e) {
+	const searchSubmitHandler = function (e) {
 		// If we've got an onSearching prop then cancel the default search behaviour
 		// and call the onSearcing callback with the query value.
-		const { onSearching } = this.props;
+		const { onSearching } = props;
 		if (onSearching) {
 			const onSearchingCallback = getCallbackFunction(onSearching);
 
@@ -62,45 +47,68 @@ export default class Search extends Component {
 		}
 
 		return false;
-	}
+	};
 
-	render() {
-		return (
-			// eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
-			<form
-				id="global-nav-search-form"
-				role="search"
-				action={this.props.url}
-				className={styles.search}
-				onSubmit={this.searchSubmitHandler}
-				onKeyDown={this.keyDownHandler}
+	// TODO: Remove this hack to fix https://github.com/alphagov/accessible-autocomplete/issues/434
+	// We do this to make our axe tests pass
+	// Wait for the search box to appear before removing the aria-activedescendant attribute
+	const searchFormRef = useCallback((node) => {
+		let searchInput;
+		if (node && "MutationObserver" in window) {
+			new MutationObserver(() => {
+				searchInput =
+					searchInput || document.querySelector(searchInputSelector);
+				if (
+					searchInput &&
+					searchInput.getAttribute("aria-activedescendant") === "false"
+				) {
+					searchInput.setAttribute("aria-activedescendant", "");
+				}
+			}).observe(node, {
+				attributeFilter: ["aria-activedescendant"],
+				attributes: true, // See https://stackoverflow.com/a/50593541/486434
+				childList: true,
+				subtree: true,
+			});
+		}
+	}, []);
+
+	return (
+		// eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
+		<form
+			id="global-nav-search-form"
+			role="search"
+			action={props.url}
+			className={styles.search}
+			onSubmit={searchSubmitHandler}
+			onKeyDown={keyDownHandler}
+			ref={searchFormRef}
+		>
+			{/* eslint jsx-a11y/label-has-for: 0 */}
+			<label className={styles.label} htmlFor="autocomplete">
+				{props.placeholder}
+			</label>
+			<Autocomplete
+				source={props.autocomplete?.suggestions || props.autocomplete}
+				suggestionTemplate={props.autocomplete?.suggestionTemplate}
+				placeholder={props.placeholder}
+				query={props.query}
+				onNavigating={props.onNavigating}
+			/>
+			<button
+				className={styles.button}
+				type="submit"
+				aria-label="Perform search"
+				onClick={searchSubmitHandler}
 			>
-				{/* eslint jsx-a11y/label-has-for: 0 */}
-				<label className={styles.label} htmlFor="autocomplete">
-					{this.props.placeholder}
-				</label>
-				<Autocomplete
-					source={
-						this.props.autocomplete?.suggestions || this.props.autocomplete
-					}
-					suggestionTemplate={this.props.autocomplete?.suggestionTemplate}
-					placeholder={this.props.placeholder}
-					query={this.props.query}
-					onNavigating={this.props.onNavigating}
-				/>
-				<button
-					className={styles.button}
-					type="submit"
-					aria-label="Perform search"
-					onClick={this.searchSubmitHandler}
-				>
-					<SearchIcon className={styles.icon} />
-				</button>
-				<SkipLink to={`#${this.props.skipLinkId}`}>Skip to content</SkipLink>
-			</form>
-		);
-	}
-}
+				<SearchIcon className={styles.icon} />
+			</button>
+			<SkipLink to={`#${props.skipLinkId}`}>Skip to content</SkipLink>
+		</form>
+	);
+};
+
+export default Search;
 
 Search.propTypes = {
 	url: PropTypes.string,
