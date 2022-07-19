a8b6e1f0897d73f7a1e8947d4286e08e4ffc3d63
Ian Routledge <ian.routledge@nice.org.uk>
2022-07-19 14:47:37 +0100

    GN-187 Fix tokens import paths

--- webpack.config.js
+++ webpack.config.js
@@ -92,7 +92,6 @@ module.exports = function (env, argv) {
 								// if you want to edit SASS in Chrome DevTools
 								sourceMap: true,
 								sassOptions: {
-									includePaths: ["./src/styles"],
 									outputStyle: "compressed",
 								},
 							},
