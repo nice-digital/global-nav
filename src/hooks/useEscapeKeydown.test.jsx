ff3198423403e4b7617ecbc858035140f20540ab
John Davey <john.davey@nice.org.uk>
2022-09-06 10:45:33 +0100

    GN-192 Simplify unit test setup for useEscapeKeydown hook

--- src/hooks/useEscapeKeydown.test.jsx
+++ src/hooks/useEscapeKeydown.test.jsx
@@ -4,44 +4,30 @@ import { mount } from "enzyme";
 import useEscapeKeydown from "./useEscapeKeydown";
 
 describe("useEscapeKeydown hook", () => {
-	beforeEach(() => {
-		// Avoid `attachTo: document.body` Warning
-		const div = document.createElement("div");
-		div.setAttribute("id", "container");
-		document.body.appendChild(div);
-	});
+	let callbackFunction;
 
-	afterEach(() => {
-		const div = document.getElementById("container");
-		if (div) {
-			document.body.removeChild(div);
-		}
-	});
+	function MyWrapper() {
+		callbackFunction = jest.fn();
+		const { ref } = useEscapeKeydown(callbackFunction);
+
+		return <div ref={ref}></div>;
+	}
 
 	it("Returns true when keydown event is the result of escape key", () => {
-		const callbackFunction = jest.fn();
-
-		function MyWrapper() {
-			const { ref } = useEscapeKeydown(callbackFunction);
-
-			return (
-				<>
-					<ul ref={ref}>
-						<li>some test item</li>
-					</ul>
-				</>
-			);
-		}
-
-		mount(<MyWrapper />, { attachTo: document.getElementById("container") });
+		mount(<MyWrapper />);
 
 		const event = new KeyboardEvent("keydown", { key: "Escape" });
 		document.dispatchEvent(event);
 
 		expect(callbackFunction).toHaveBeenCalledTimes(1);
+	});
 
-		const event2 = new KeyboardEvent("keydown", { key: "Enter" });
-		document.dispatchEvent(event2);
-		expect(callbackFunction).toHaveBeenCalledTimes(1);
+	it("Returns false when keydown event is not the result of escape key", () => {
+		mount(<MyWrapper />);
+
+		const event = new KeyboardEvent("keydown", { key: "Enter" });
+		document.dispatchEvent(event);
+
+		expect(callbackFunction).not.toHaveBeenCalled();
 	});
 });
