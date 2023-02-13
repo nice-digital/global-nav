50134e47f7a062d5feafcf02130c6934e0b67327
John Davey <john.davey@nice.org.uk>
2023-02-13 09:16:12 +0000

    GN-203 Change text on HTE programme

--- src/Header/Nav/Dropdown/Components/Guidance.jsx
+++ src/Header/Nav/Dropdown/Components/Guidance.jsx
@@ -181,8 +181,8 @@ export function Guidance({ rootUrl }) {
 									</a>
 								</h4>
 								<p className="mt--0">
-									Review evidence to support adoption of health technologies in
-									the NHS.
+									Exploring new ways to support adoption of health technologies
+									in the NHS, including early value assessment.
 								</p>
 							</li>
 
