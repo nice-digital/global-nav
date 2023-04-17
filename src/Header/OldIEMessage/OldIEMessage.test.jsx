b2d8066c25cd9d1f1b6ab5267bfbbb4c1dbd52b1
Ian Routledge <ian.routledge@nice.org.uk>
2023-04-17 20:27:22 +0100

    GN-76 Fix tests

--- src/Header/OldIEMessage/OldIEMessage.test.jsx
+++ src/Header/OldIEMessage/OldIEMessage.test.jsx
@@ -2,39 +2,41 @@ import React from "react";
 import OldIEMessage from "./OldIEMessage";
 import { render } from "@testing-library/react";
 
+import { getIEVersion } from "./ie";
+
+jest.mock("./ie", () => ({
+	getIEVersion: jest.fn().mockReturnValue(undefined),
+}));
+
 describe("OldIEMessage", () => {
 	it("Doesn't render in IE11", () => {
+		getIEVersion.mockReturnValue(11);
+
 		const { container } = render(<OldIEMessage />);
 
-		wrapper.instance().getIEVersion = jest.fn(() => 11);
-		wrapper.instance().forceUpdate();
-
-		expect(wrapper.get(0)).toBeNull();
+		expect(container).toBeEmptyDOMElement();
 	});
 
 	it("Doesn't render in modern browsers", () => {
+		getIEVersion.mockReturnValue(undefined);
+
 		const { container } = render(<OldIEMessage />);
 
-		wrapper.instance().getIEVersion = jest.fn(() => null);
-		wrapper.instance().forceUpdate();
-
-		expect(wrapper.get(0)).toBeNull();
+		expect(container).toBeEmptyDOMElement();
 	});
 
 	it("Matches snapshot in IE 10", () => {
-		const { container } = render(<OldIEMessage />);
+		getIEVersion.mockReturnValue(10);
 
-		wrapper.instance().getIEVersion = jest.fn(() => 10);
-		wrapper.instance().forceUpdate();
+		const { container } = render(<OldIEMessage />);
 
 		expect(container).toMatchSnapshot();
 	});
 
 	it("Matches snapshot in IE 8/9", () => {
-		const { container } = render(<OldIEMessage />);
+		getIEVersion.mockReturnValue(9);
 
-		wrapper.instance().getIEVersion = jest.fn(() => 9);
-		wrapper.instance().forceUpdate();
+		const { container } = render(<OldIEMessage />);
 
 		expect(container).toMatchSnapshot();
 	});
--- src/Header/Search/Autocomplete/Autocomplete.test.jsx
+++ src/Header/Search/Autocomplete/Autocomplete.test.jsx
@@ -1,16 +1,35 @@
 import React from "react";
-import { getByRole, render, waitFor } from "@testing-library/react";
+import { render, waitFor, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import Autocomplete, { rateLimitWait } from "./Autocomplete";
 
 import { eventName, eventTimeout } from "./../../../tracker";
 
+import { suggester } from "./suggester";
+
+jest.mock("./suggester", () => ({
+	suggester: jest.fn(jest.requireActual("./suggester").suggester),
+}));
+
+//let windowAssignMock = jest.fn();
+
+//delete window.location;
+//window.location = { assign: windowAssignMock };
+
 describe("Autocomplete", () => {
 	const defaultProps = {};
 
+	window.location = { ...window.location, assign: jest.fn() };
+
 	beforeEach(() => {
 		window.dataLayer = [];
+
+		suggester.mockClear();
+	});
+
+	afterEach(() => {
+		window.location.assign.mockClear();
 	});
 
 	describe("Autocomplete", () => {
@@ -71,6 +90,7 @@ describe("Autocomplete", () => {
 			expect(suggestionTemplate.mock.calls[0][0]).toStrictEqual({
 				...option,
 				TitleHtml: "<mark>dia</mark>betes type 1",
+				TypeAheadType: undefined,
 			});
 		});
 
@@ -106,7 +126,7 @@ describe("Autocomplete", () => {
 			]);
 		});
 
-		it.only("should push autocomplete select event to the dataLayer using TypeAheadType property", async () => {
+		it("should push autocomplete select event to the dataLayer using TypeAheadType property", async () => {
 			const option = {
 				Title: "diabetes type 1",
 				TypeAheadType: "keyword",
@@ -139,52 +159,55 @@ describe("Autocomplete", () => {
 			]);
 		});
 
-		it("should navigate to selected option in event callback", () => {
-			document.body.innerHTML = "";
-			var appContainer = document.createElement("div");
-			document.body.appendChild(appContainer);
-
+		it("should navigate to selected option in event callback", async () => {
 			const option = {
 				Title: "diabetes type 1",
 				Link: "https://www.nice.org.uk/diabetes1.html",
 			};
 
-			const wrapper = mount(
-				<Autocomplete {...defaultProps} source={[option]} query="diab" />,
-				{ attachTo: appContainer }
-			);
+			const { getByRole } = render(
+					<Autocomplete {...defaultProps} source={[option]} query="dia" />
+				),
+				input = getByRole("combobox"),
+				user = userEvent.setup();
 
-			wrapper.find("#autocomplete").first().props().onConfirm(option);
+			await user.type(input, "diab");
+
+			await waitFor(async () => {
+				const optionLink = getByRole("link", { name: "diab etes type 1" });
+				await user.click(optionLink);
+			});
 
 			window.dataLayer[0].eventCallback();
-			expect(window.location.href).toEqual(
+			expect(window.location.assign).toHaveBeenCalledWith(
 				"https://www.nice.org.uk/diabetes1.html"
 			);
 		});
 
-		it("should call onNavigating prop function with selected option in event callback", () => {
-			document.body.innerHTML = "";
-			var appContainer = document.createElement("div");
-			document.body.appendChild(appContainer);
-
+		it("should call onNavigating prop function with selected option in event callback", async () => {
 			const option = {
-				Title: "diabetes type 1",
-				Link: "/diabetes1.html",
-			};
+					Title: "diabetes type 1",
+					Link: "/diabetes1.html",
+				},
+				onNavigating = jest.fn();
 
-			const onNavigating = jest.fn();
+			const { getByRole } = render(
+					<Autocomplete
+						{...defaultProps}
+						source={[option]}
+						query="dia"
+						onNavigating={onNavigating}
+					/>
+				),
+				input = getByRole("combobox"),
+				user = userEvent.setup();
 
-			const wrapper = mount(
-				<Autocomplete
-					{...defaultProps}
-					source={[option]}
-					query="diab"
-					onNavigating={onNavigating}
-				/>,
-				{ attachTo: appContainer }
-			);
+			await user.type(input, "diab");
 
-			wrapper.find("#autocomplete").first().props().onConfirm(option);
+			await waitFor(async () => {
+				const optionLink = getByRole("link", { name: "diab etes type 1" });
+				await user.click(optionLink);
+			});
 
 			window.dataLayer[0].eventCallback();
 			expect(onNavigating).toHaveBeenCalledWith({
@@ -193,45 +216,29 @@ describe("Autocomplete", () => {
 			});
 		});
 
-		it("should not load suggestions within the rate limit threshold", () => {
-			jest.useFakeTimers();
-			const { container } = render(
-				<Autocomplete {...defaultProps} source="/url" query="test" />
-			);
-			const suggestSpy = jest
-				.spyOn(wrapper.instance(), "suggest")
-				.mockImplementation(() => {});
-			wrapper.instance().forceUpdate();
-			const source = wrapper.find("#autocomplete").props().source;
+		it("should debounce suggestions", async () => {
+			const option = {
+				Title: "diabetes type 1",
+				Link: "/diabetes1.html",
+			};
 
-			source("que", () => {});
-			jest.advanceTimersByTime(rateLimitWait - 1);
-			source("quer", () => {});
+			const { getByRole, queryAllByRole } = render(
+					<Autocomplete {...defaultProps} source={[option]} />
+				),
+				input = getByRole("combobox"),
+				user = userEvent.setup();
 
-			expect(suggestSpy).not.toBeCalled();
-		});
+			await user.type(input, "dia");
+			await user.type(input, "b");
+			await user.type(input, "e");
 
-		it("should load the suggestions only once after the rate limit threshold", () => {
-			jest.useFakeTimers();
-			const callback = () => {};
-			const { container } = render(
-				<Autocomplete {...defaultProps} source="/url" query="test" />
-			);
-			const suggestSpy = jest
-				.spyOn(wrapper.instance(), "suggest")
-				.mockImplementation(() => {});
-			wrapper.instance().forceUpdate();
-			const source = wrapper.find("#autocomplete").props().source;
+			await waitFor(() => {
+				const optionLinks = queryAllByRole("link");
+				expect(optionLinks).toHaveLength(1);
+			});
 
-			source("que", callback);
-			jest.advanceTimersByTime(rateLimitWait - 10);
-			source("quer", callback);
-			jest.advanceTimersByTime(rateLimitWait - 5);
-			source("query", callback);
-
-			jest.advanceTimersByTime(rateLimitWait);
-			expect(suggestSpy).toBeCalledTimes(1);
-			expect(suggestSpy).toBeCalledWith("query", callback);
+			expect(suggester).toBeCalledTimes(1);
+			expect(suggester).toBeCalledWith([option], "diabe", 5);
 		});
 	});
 
