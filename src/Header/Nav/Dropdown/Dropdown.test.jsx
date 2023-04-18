288caad19230423fdbe0618e78d21ee1de6bfdcc
Ian Routledge <ian.routledge@nice.org.uk>
2023-04-18 19:51:34 +0100

    GN-76 Add dropdown tests

--- src/Header/Nav/Dropdown/Dropdown.test.jsx
+++ src/Header/Nav/Dropdown/Dropdown.test.jsx
@@ -1,72 +1,74 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { createEvent, fireEvent, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Dropdown } from "./Dropdown";
 
 jest.mock("../../../services.json", () =>
 	require("./../__mocks__/services.json")
 );
 
-const DropdownComponent = () => null;
+const DropdownComponent = () => <div data-testid="dropdown-component"></div>;
 
 describe("Dropdown", () => {
 	const defaultProps = {
-		text: "Our Dropdown",
+		text: "Test section dropdown",
 		id: "link2",
 		component: DropdownComponent,
 	};
 
 	it("Should have a data-tracking attribute on the wrapper", () => {
 		const { container } = render(<Dropdown {...defaultProps} />);
-		const trackingDiv = wrapper.find("div[data-tracking]");
-		expect(trackingDiv.length).toEqual(1);
-		expect(trackingDiv.props()["data-tracking"]).toEqual(
-			"Our Dropdown dropdown"
+		expect(container.firstChild).toHaveAttribute(
+			"data-tracking",
+			"Test section dropdown dropdown"
 		);
 	});
 
 	it("should render the given component", () => {
-		const { container } = render(<Dropdown {...defaultProps} />);
-		expect(
-			wrapper.find("[rootUrl='https://www.test-link2.nice.org']").length
-		).toEqual(1);
+		const { getByTestId } = render(<Dropdown {...defaultProps} />);
+		expect(getByTestId("dropdown-component")).toBeInTheDocument();
 	});
 
 	it("Should render a skip link when there is a next nav item to skip to", () => {
-		const closingFunction = jest.fn();
-		const preventer = jest.fn();
-		const { container } = render(
-			<Dropdown
-				{...defaultProps}
-				nextNavSlug="next-thing"
-				closeDropdown={closingFunction}
-			/>
-		);
-		const skipButton = wrapper.find("a[href='#navlink-next-thing']");
-		expect(skipButton.length).toEqual(1);
-		expect(skipButton.text()).toBe("Skip Our Dropdown submenu");
-		skipButton
-			.props()
-			.onClick({ preventDefault: preventer, target: { hash: "#myButton" } });
+		const closingFunction = jest.fn(),
+			{ getByRole } = render(
+				<Dropdown
+					{...defaultProps}
+					nextNavSlug="next-thing"
+					closeDropdown={closingFunction}
+				/>
+			),
+			skipLink = getByRole("link", {
+				name: "Skip Test section dropdown submenu",
+			}),
+			clickEvent = createEvent.click(skipLink);
+
+		fireEvent(skipLink, clickEvent);
+
 		expect(closingFunction).toHaveBeenCalled();
-		expect(preventer).toHaveBeenCalled();
+		expect(clickEvent.defaultPrevented).toBe(true);
 	});
 
 	it("Should not render a skip link when there is no next nav item to skip to", () => {
-		const { container } = render(<Dropdown {...defaultProps} />);
-		expect(wrapper.find("a[href='#navlink-next-thing']").length).toEqual(0);
+		const { queryByRole } = render(<Dropdown {...defaultProps} />);
+		expect(
+			queryByRole("link", { name: "Skip Test section dropdown submenu" })
+		).toBeNull();
 	});
 
-	it("should fire onClosing when the close dropdown button is clicked", () => {
+	it("should fire onClosing when the close dropdown button is clicked", async () => {
 		const closingFunction = jest.fn();
 
-		const { container } = render(
-			<Dropdown {...defaultProps} closeDropdown={closingFunction} />
-		);
+		const { getByRole } = render(
+				<Dropdown {...defaultProps} closeDropdown={closingFunction} />
+			),
+			closeButton = getByRole("button", { name: "Close menu" }),
+			user = userEvent.setup();
 
-		const closeButton = wrapper.find("button");
-		expect(closeButton.props()["data-tracking"]).toEqual("Close menu");
+		expect(closeButton).toHaveAttribute("data-tracking", "Close menu");
 
-		closeButton.props().onClick();
+		await user.click(closeButton);
+
 		expect(closingFunction).toHaveBeenCalled();
 	});
 });
