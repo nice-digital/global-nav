ad90364db24126b4aec8227fcef8aeca117b1bb6
John Davey <john.davey@nice.org.uk>
2023-09-27 13:53:58 +0100

    redirect to /examples/ in dev mode

--- src/cdn.js
+++ src/cdn.js
@@ -4,3 +4,18 @@ console.log(`server in ${import.meta.env.MODE.toUpperCase()} mode`);
 renderHeader();
 
 renderFooter();
+
+// Check if the redirection flag is not set and the environment is development to show html examples
+if (
+	!localStorage.getItem("redirected") &&
+	import.meta.env.MODE === "development"
+) {
+	// Set the redirection flag
+	localStorage.setItem("redirected", "true");
+
+	// Redirect to /examples/ in development mode
+	window.location.href = "/examples/";
+} else {
+	// Clear the redirection flag from localStorage
+	localStorage.removeItem("redirected");
+}
