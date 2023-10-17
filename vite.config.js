3dba76adfb2ad030682e657fdc165fe77f5ca34f
John Davey <john.davey@nice.org.uk>
2023-10-17 14:27:20 +0100

    Remove command and ssrBuild params from vite defineConfig function

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ const outDir = resolve(__dirname, "dist");
 
 // https://vitejs.dev/config/
 
-export default defineConfig(({ command, mode, ssrBuild }) => {
+export default defineConfig(({ mode }) => {
 	// Load env file based on `mode` in the current working directory.
 	// Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
 	const env = loadEnv(mode, process.cwd(), "");
