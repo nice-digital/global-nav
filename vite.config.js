b3c82937d154c00b66c540367d515bd05604a1c1
John Davey <john.davey@nice.org.uk>
2023-10-18 09:44:13 +0100

    GN-218 Add cross env back to npm build script. Comment out define block in vite config

--- vite.config.js
+++ vite.config.js
@@ -91,12 +91,12 @@ export default defineConfig(({ mode }) => {
 				},
 			},
 		},
-		define: {
-			"process.env.VERSION":
-				process.env.NODE_ENV === "production"
-					? process.env.VITE_APP_VERSION
-					: JSON.stringify(process.env.VITE_APP_VERSION),
-		},
+		// define: {
+		// 	__APP_VERSION__:
+		// 		process.env.NODE_ENV === "production"
+		// 			? process.env.VITE_APP_VERSION
+		// 			: JSON.stringify(process.env.VITE_APP_VERSION),
+		// },
 		test: {
 			globals: true,
 			restoreMocks: true,
