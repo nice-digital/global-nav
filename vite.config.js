e3edbcefd4b6c0299abc0f6de2b0dcdc5501eaeb
John Davey <john.davey@nice.org.uk>
2023-10-24 12:16:11 +0100

    GN-218 Add BUILD_NUMBER to banner - wip

--- vite.config.js
+++ vite.config.js
@@ -19,9 +19,9 @@ export default defineConfig(({ mode }) => {
 	// if command === build - you can return a production mode specific config
 
 	const bannerContent = [
-		`NICE Global Nav ${env.VITE_APP_VERSION || packageJson.version} | ${
-			new Date().toISOString().split("T")[0]
-		}`,
+		`NICE Global Nav ${packageJson.version} | build ${
+			env.BUILD_NUMBER || `N/A`
+		} | ${new Date().toISOString().split("T")[0]}`,
 		`© Copyright NICE 2015-${new Date().getFullYear()}`,
 		"Licensed under MIT (https://github.com/nice-digital/global-nav/blob/master/LICENSE)",
 	].join("\n");
