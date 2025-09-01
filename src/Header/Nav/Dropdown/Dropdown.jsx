a283d0e7d9133a935c1d93da0debbb6fe80d1aaf
beccadawson <rd_x@live.com>
2025-09-01 13:22:04 +0100

    GN-237 update header nav to only display items marked for inclusion in header nav and regenerate snapshot to reflect changes (#236)
    * GN-237 update header nav to only display items marked for inclusion in header nav and regenerate snapshot to reflect changes
    
    * GN-237 Remove filter for header links from NavLinks as this is now done when creating ServicesToDisplay array in Nav
    
    * GN-237 update Skip Dropdown link behaviour to be more consistent with SkipLink to fix intermittent focus bug
    
    * GN-237 add fix for font size issue on some versions of global nav
    > Henry Cass Yesterday 13:47
    > just spotted what looks like a font-size issue in the meganav on api.nice.org.uk
    
    * GN-237 update dropdown skiplink unit test to reflect update to skiplink logic

--- src/Header/Nav/Dropdown/Dropdown.jsx
+++ src/Header/Nav/Dropdown/Dropdown.jsx
@@ -22,12 +22,19 @@ export function Dropdown({
 	const focusTrapRef = useFocusTrap(idOfOpenDropdown !== null);
 
 	function handleSkipLink(e) {
-		e.preventDefault();
-		const element = document.getElementById(e.target.hash.substr(1));
-		requestAnimationFrame(() => {
-			element && element.focus();
-		});
-		closeDropdown();
+		const href = e.currentTarget.getAttribute("href");
+
+		if (href && href.indexOf("#") === 0) {
+			const id = href.split("#")[1],
+				element = document.getElementById(id);
+
+			if (element) {
+				e.preventDefault();
+				element.setAttribute("tabIndex", "-1");
+				element.focus();
+				closeDropdown();
+			}
+		}
 	}
 
 	const { host } = services.external.find((service) => service.id == id);
